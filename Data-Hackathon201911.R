dcb5329bed80046f2285db5478c3cc7e86f8b57b
jennifer siwu <jsiwu@uci.edu>
2019-11-09 23:30:51 -0800

    a

--- Data-Hackathon201911.R
+++ Data-Hackathon201911.R
@@ -8,6 +8,7 @@ library(tidyverse)
 library(rmarkdown)
 library(stats)
 library(gmodels)
+library(randomForest)
 
 #start reading the data
 df <- read.csv(file = "~/Documents/Data-Hackathon-ModelTeam/bank_full_cleaned.csv"
@@ -15,13 +16,13 @@ df <- read.csv(file = "~/Documents/Data-Hackathon-ModelTeam/bank_full_cleaned.cs
 head(df)
 colnames(df)
 summary(df, na.rm = TRUE)
+view(df)
 
-
+#overall baseline
 ##quick skim through of the freq dist
 job <- table(df['job'])
-CrossTable(df$job)
 marital <- table(df['marital'])
-CrossTable(df$marital)
+
 
 age <- data.frame(Variable.name = "age",
            df %>%
@@ -40,6 +41,86 @@ normalize <- function(x){
 }
 
 
+####Model
+train <- read.csv(file = "~/Documents/Data-Hackathon-ModelTeam/data_normalised.csv"
+                  ,header=TRUE,sep=",")
+
+set.seed(100)
+train_s <- sample_n(train[train$y == 0,], 5289)
+train_y <- train[train$y == 1,]
+
+combined <- rbind(train_s,train_y)
+rownames(combined) <- seq(length=nrow(combined))
+
+to_csv('')
+
+split <- sample(nrow(combined), 0.8*nrow(combined))
+train_m <- combined[split,]
+test_m <- combined[-split,]
+
+colnames(train_m)
+
+bank.logistic <- glm(y_yes~., 
+                     data = train_m, family = binomial)
+
+#predict:: train
+predictions.logistic.train <- predict(bank.logistic,train_m,type = "response")
+predictions.logistic.binary.train <- ifelse(predictions.logistic.train > .5,"yes","no")
+
+#predict:: test
+predictions.logistic.test <- predict(bank.logistic,test_m,type = "response")
+predictions.logistic.binary.test <- ifelse(predictions.logistic.test > .5,"yes","no")
+
+#Confusion matrix::
+con.mat.log.train <- prop.table(table(train_m$y,predictions.logistic.binary.train))
+con.mat.log.test <- prop.table(table(test_m$y,predictions.logistic.binary.test))
+
+table(test_m$y,predictions.logistic.binary.test)
+
+
+1-((172+169)/(172+169+908+867))
+
+
+
+
+# Display variable importance measures for a random forest model
+importance(forest, type=2)
+
+# predict the target using the evaluate data
+forest.pred <- predict(forest, df.validate)
+forest.perf <- table(df.validate$target, forest.pred,
+                     dnn=c("Actual", "Predicted"))
+forest.perf
+
+
+
+##plotting
+
+#descriptive stat::
+ggplot(df, aes(x=job, fill = y)) + geom_bar() +
+  ggtitle("Job types vs decision",size = 20) + xlab("Job Types",size = 20) + 
+  ylab("Title",size = 20) + labs(fill= "y")
+
+ggplot(df, aes(x=education, fill = y)) + geom_bar() + facet_wrap(~marital)+
+  ggtitle("Marital status vs education") + xlab("Educational qualification") + 
+  ylab("Count") + labs(fill= "y")
+
+ggplot(df, aes(x=housing, fill = y)) + geom_bar() + facet_wrap(~default)+
+  ggtitle("Housing loan vs default") + xlab("Housing loan") + 
+  ylab("Count") + labs(fill= "y")
+
+ggplot(df, aes(x=loan, fill = y)) + geom_bar() + facet_wrap(~default)+
+  ggtitle("Personal loan vs default") + xlab("Personal loan") + 
+  ylab("Count") + labs(fill= "y")
+
+
+ggplot(df,aes(x=poutcome, fill = y)) + geom_bar() +
+  ggtitle("Success vs previous outcome") + xlab("Previous outcome") + 
+  ylab("Count") + labs(fill= "y")
+
+
+
+table(df$job)
 ##Dummy Vars for Job
 df['admin'] <- ifelse(df$job == "admin.",1,0)
 df['blue-collar']<- ifelse(df$job == "blue-collar",1,0)
@@ -47,7 +128,7 @@ df['entrepreneur']<- ifelse(df$job == "entrepreneur",1,0)
 df['housemaid']<- ifelse(df$job == "housemaid",1,0)
 df['management']<- ifelse(df$job == "management",1,0)
 df['retired']<- ifelse(df$job == "retired",1,0)
-df['self-employed']<- ifelse(df$job == "self-employed ",1,0)
+df['self-employed']<- ifelse(df$job == "self-employed",1,0)
 df['services']<- ifelse(df$job == "services",1,0)
 df['student']<- ifelse(df$job == "student",1,0)
 df['technician']<- ifelse(df$job == "technician",1,0)
@@ -60,7 +141,17 @@ df['married']<- ifelse(df$marital == "married",1,0)
 df['single']<- ifelse(df$marital == "single",1,0)
 df['normage'] <- normalize(df$age)
 
+df['dependent']<-ifelse(df$y == "yes",1,0)
+
+df['pdays_neg1'] <- ifelse(df$pdays <= -1,1,0)
+
+
+df['zero_previous'] <- ifelse(df$previous == 0,1,0)
+df['unknown_poutcome'] <- ifelse(df$poutcome == -1,1,0)
+
+
 #all independent var cortest
 df_cor <- df[,18:33]
 cortest <- data.frame(cor(df_cor, method="pearson",use="complete.obs"))
 view(cortest)
+
